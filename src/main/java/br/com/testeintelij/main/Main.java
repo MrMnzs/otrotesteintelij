46cd3e8e254bb93fda8bc34c08594bfbfec84c46
MrMnzs <andremnzs@gmail.com>
2022-05-31 12:26:19 -0300

    alteracao na mains linha 8

--- src/main/java/br/com/testeintelij/main/Main.java
+++ src/main/java/br/com/testeintelij/main/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("olla ");
         System.out.println("outro teste");
+        System.out.println("testet1111");
     }
 }
