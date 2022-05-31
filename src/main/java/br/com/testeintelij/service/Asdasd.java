724bab4d649733ec20601c1734c1b7cf13487fd5
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-05-31 12:21:16 -0300

    criei classe nova

---
+++ src/main/java/br/com/testeintelij/service/Asdasd.java
@@ -0,0 +1,9 @@
+package br.com.testeintelij.service;
+
+public class Asdasd {
+
+    public static void main(String[] args) {
+        System.out.println("asdasdasd");
+        System.out.println("asdasdas");
+    }
+}
